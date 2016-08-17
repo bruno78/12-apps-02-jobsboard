b5f2997b39c62f06c22aba56569f89301a26edde
bruno78 <devbruno78@gmail.com>
2016-08-17 14:40:52 -0400

    added description to model, updated rake, and jobs new form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,10 @@
 class JobsController < ApplicationController
+  before_action :find_job, only: [:show, :edit, :update, :destroy]
   def index
     @jobs = Job.page(params[:page]).per(25)
+  end
+
+  def show
   end
 
   def new
@@ -8,11 +12,30 @@ class JobsController < ApplicationController
   end
 
   def create
-    @job = Job.new(params.require(:job).permit(:title, :company, :url))
+    @job = Job.new(params.require(:job).permit(:title, :descripton, :company, :url))
     if @job.save
       redirect_to root_path
     else
-      render "new"
+      render "New"
     end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def jobs_params
+    params.require(:job).perimit(:title, :description, :company, :url)
+  end
+
+  def find_job
+    @job = Job.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224203721) do
+ActiveRecord::Schema.define(version: 20160817183319) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.string   "company"
     t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
 end
